6a9e7ead5d12cb29239b88d2aa520925b9ec882f
ceballosAgustin <aceballos1923@gmail.com>
2024-04-02 22:58:39 -0300

    Ĺógica página principal

---
+++ prototipo-asp/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+
+namespace prototipo_asp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["listaDirecciones"] == null)
+            {
+                DireccionNegocio negocio = new DireccionNegocio();
+                Session.Add("listaDirecciones", negocio.listar());
+            }
+
+            dgvDirecciones.DataSource = Session["listaDirecciones"];
+            dgvDirecciones.DataBind();
+
+        }
+
+        protected void dgvDirecciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var id = dgvDirecciones.SelectedDataKey.Value.ToString();
+            Response.Redirect("DireccionForm.aspx?id=" + id);
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("DireccionForm.aspx");
+        }
+    }
+}
